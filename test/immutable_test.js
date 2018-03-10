a6a0f715af448d830525e083c7c0bcb68e3ab05a
Mark McGranaghan <mmcgrana@gmail.com>
2018-03-10 03:43:29 -0800

    Add a few read-side tests

--- test/immutable_test.js
+++ test/immutable_test.js
@@ -4,50 +4,6 @@ const Automerge = require('../src/Automerge')
 
 const ROOT_ID = '00000000-0000-0000-0000-000000000000'
 
-/*
-describe('Automerge.initImmutable()', () => {
-  let beforeDoc, afterDoc, appliedDoc, appliedDoc2, changes
-
-  beforeEach(() => {
-    beforeDoc = Automerge.change(Automerge.initImmutable(), doc => doc.document = 'watch me now')
-    afterDoc = Automerge.change(beforeDoc, doc => doc.document = 'i can mash potato')
-    changes = Automerge.getChanges(beforeDoc, afterDoc)
-    appliedDoc = Automerge.applyChanges(beforeDoc, changes)
-    appliedDoc2 = Automerge.applyChanges(appliedDoc, changes)
-  })
-
-  it('Uses Immutable.Map', () => {
-    assert(beforeDoc instanceof Immutable.Map)
-    assert(afterDoc instanceof Immutable.Map)
-    assert(appliedDoc instanceof Immutable.Map)
-    assert(appliedDoc2 instanceof Immutable.Map)
-  })
-
-  it('applies changes', () => {
-    assert.equal(Automerge.save(appliedDoc), Automerge.save(afterDoc))
-    assert.equal(Automerge.save(appliedDoc2), Automerge.save(afterDoc))
-  })
-
-  it('supports fetching conflicts on lists', () => {
-    let s1 = Automerge.change(Automerge.initImmutable(), doc => doc.pixels = ['red'])
-    let s2 = Automerge.merge(Automerge.initImmutable(), s1)
-    s1 = Automerge.change(s1, doc => doc.pixels[0] = 'green')
-    s2 = Automerge.change(s2, doc => doc.pixels[0] = 'blue')
-    s1 = Automerge.merge(s1, s2)
-    if (s1._actorId > s2._actorId) {
-      assert(s1.get('pixels').equals(Immutable.List.of('green')))
-      assert(Automerge.getConflicts(s1, s1.get('pixels')).equals(
-        Immutable.List.of(Immutable.Map().set(s2._actorId, 'blue'))))
-    } else {
-      assert(s1.get('pixels').equals(Immutable.List.of('blue')))
-      assert(Automerge.getConflicts(s1, s1.get('pixels')).equals(
-        Immutable.List.of(Immutable.Map().set(s1._actorId, 'green'))))
-    }
-  })
-})
-*/
-
-
 describe('Immutable write interface', () => {
   // TODO: correct public API?
   it('has a fixed object ID at the root', () => {
@@ -384,4 +340,44 @@ describe('Immutable write interface', () => {
   //       '{"out":{"in":3},"_objectId":"00000000-0000-0000-0000-000000000000"}')
   //   })
   // })
+})
+
+
+describe('Immutable read interface', () => {
+  it('uses Immutable.Map for outer and inner objects', () => {
+    const doc1 = Automerge.initImmutable()
+    const doc2 = Automerge.change(doc1, doc => {
+      return doc.set('outer', new Map().set('inner', 'foo'))
+    })
+    assert(doc2 instanceof Map)
+    assert(doc2.get('outer') instanceof Map)
+  })
+
+  // TODO: do we actually want this?
+  // TODO: is an empty _conflicts expected?
+  it('includes Automerge-provided keys in maps', () => {
+    const doc1 = Automerge.initImmutable()
+    const doc2 = Automerge.change(doc1, doc => {
+      return doc.set('outer', new Map().set('inner', 'foo'))
+    })
+    assert(doc2.keySeq().toSet().equals(new Set(['_objectId', '_conflicts', 'outer'])))
+    assert(doc2.get('outer').keySeq().toSet().equals(new Set(['_objectId', 'inner'])))
+  })
+
+  //it('supports fetching conflicts on lists', () => {
+  //  let s1 = Automerge.change(Automerge.initImmutable(), doc => doc.pixels = ['red'])
+  //  let s2 = Automerge.merge(Automerge.initImmutable(), s1)
+  //  s1 = Automerge.change(s1, doc => doc.pixels[0] = 'green')
+  //  s2 = Automerge.change(s2, doc => doc.pixels[0] = 'blue')
+  //  s1 = Automerge.merge(s1, s2)
+  //  if (s1._actorId > s2._actorId) {
+  //    assert(s1.get('pixels').equals(Immutable.List.of('green')))
+  //    assert(Automerge.getConflicts(s1, s1.get('pixels')).equals(
+  //      Immutable.List.of(Immutable.Map().set(s2._actorId, 'blue'))))
+  //  } else {
+  //    assert(s1.get('pixels').equals(Immutable.List.of('blue')))
+  //    assert(Automerge.getConflicts(s1, s1.get('pixels')).equals(
+  //      Immutable.List.of(Immutable.Map().set(s1._actorId, 'green'))))
+  //  }
+  //})
 })
